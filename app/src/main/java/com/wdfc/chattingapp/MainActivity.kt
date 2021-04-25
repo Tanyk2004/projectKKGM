ac1625db2e313def0fa8eab7a9d45baef5805d88
Tanyk2004 <tanyk.marchborn24@gmail.com>
2021-04-25 15:31:43 +0530

    did the sign up activity along with some animations and shapes

--- app/src/main/java/com/wdfc/chattingapp/MainActivity.kt
+++ app/src/main/java/com/wdfc/chattingapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.wdfc.chattingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        overridePendingTransition(R.anim.fadein , R.anim.fadeout)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+
+        var txt = findViewById<TextView>(R.id.UwU)
+       txt.setOnClickListener {
+           startActivity(Intent(this, sign_up::class.java))
+           finish()
+       }
+
+}}
--- app/src/main/java/com/wdfc/chattingapp/sign_up.kt
+++ app/src/main/java/com/wdfc/chattingapp/sign_up.kt
@@ -2,10 +2,16 @@ package com.wdfc.chattingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
 
 class sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        overridePendingTransition(R.anim.righttoleft,R.anim.fadeout)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        var animation = AnimationUtils.loadAnimation(this, R.anim.zoomin)
+        var sign_up_button = findViewById<Button>(R.id.btn_sign_up)
+        sign_up_button.startAnimation(animation)
     }
 }
