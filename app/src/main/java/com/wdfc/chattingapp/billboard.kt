194aa77a570b41dce166895bd179e49fe3af05d3
Tanyk2004 <tanyk.marchborn24@gmail.com>
2021-04-30 15:26:42 +0530

    profile code

---
+++ app/src/main/java/com/wdfc/chattingapp/billboard.kt
@@ -0,0 +1,11 @@
+package com.wdfc.chattingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class billboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_billboard)
+    }
+}
