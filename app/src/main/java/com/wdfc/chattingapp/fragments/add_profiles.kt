b23352967af0c304c9f5027019c62190aee1ecf3
Tanyk2004 <tanyk.marchborn24@gmail.com>
2021-05-06 11:43:09 +0530

    add profiles completed

--- app/src/main/java/com/wdfc/chattingapp/fragments/add_profiles.kt
+++ app/src/main/java/com/wdfc/chattingapp/fragments/add_profiles.kt
@@ -17,7 +17,19 @@ class add_profiles : Fragment(R.layout.fragment_add_profiles) {
     var selectedState = ""
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //****spinner****
+        val db =  Firebase.firestore
+
+        //***initialisation of views**********
+        var name = activity?.findViewById<TextView>(R.id.txt_full_name)
+        var city = activity?.findViewById<TextView>(R.id.txt_city)
+        var service = activity?.findViewById<TextView>(R.id.txt_type_of_service)
+        var phone = activity?.findViewById<TextView>(R.id.txt_contact)
+        var additional_information = activity?.findViewById<TextView>(R.id.txt_additional_info)
+        var submit =  activity?.findViewById<Button>(R.id.btn_add)
+        //***initialisation of views**********
+
+
+        //********spinner*********
         var states = activity?.findViewById<Spinner>(R.id.spinner)
 
         var stateNames = arrayOf("Andhra Pradesh","Andaman and Nicobar" ,"Arunachal Pradesh", "Assam", "Bihar","Chandigarh", "Chattisgarh","Dadra and Nagar Haveli & Daman and Diu","Delhi",
@@ -47,15 +59,66 @@ class add_profiles : Fragment(R.layout.fragment_add_profiles) {
 
             }
             //****spinner****
+
+
         }
-        var submit =  activity?.findViewById<Button>(R.id.btn_add)
-        submit?.setOnClickListener{
-            var name = activity?.findViewById<TextView>(R.id.txt_full_name)?.text.toString()
+
+
+            submit?.setOnClickListener{
+
+            var info_profile = hashMapOf(
+                    "state" to selectedState,
+                    "name" to name?.text.toString(),
+                    "city" to city?.text.toString().trim().toLowerCase(),
+                    "service" to service?.text.toString(),
+                    "additional_info" to additional_information?.text.toString()
+
+
+            )
+            db.collection("lifesupport").add(info_profile)
+                    .addOnSuccessListener {
+                        Toast.makeText(activity,  "Your information has successfully been uploaded on the billboard" , Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(activity, "There was an error please try again" , Toast.LENGTH_SHORT).show()
+                    }
             Toast.makeText(context,  selectedState , Toast.LENGTH_SHORT).show()
-        }
+
+            }
 
     }
 
+    private fun checkEmpty() : Boolean {
+        var check = true
+        var add = add_profiles()
+        var full_name = add.view?.findViewById<TextView>(R.id.txt_full_name)
+        var service_type = add.view?.findViewById<TextView>(R.id.txt_type_of_service)
+        var city = add.view?.findViewById<TextView>(R.id.txt_city)
+        var phone_number = add.view?.findViewById<TextView>(R.id.txt_contact)
+        var additional_info = add.view?.findViewById<TextView>(R.id.txt_additional_info)
 
+
+        if (full_name?.text.toString().isEmpty()){
+            full_name?.requestFocus()
+            full_name?.error = "Please enter your name"
+            check = false
+        }
+        if (city?.text.toString().isEmpty()){
+            city?.requestFocus()
+            city?.error = "Please enter your city"
+            check = false
+        }
+        if (service_type?.text.toString().isEmpty()){
+            service_type?.requestFocus()
+            service_type?.error = "Please enter the type of support you can supply"
+            check = false
+        }
+        if (phone_number?.text.toString().isEmpty()){
+            phone_number?.requestFocus()
+            phone_number?.error = "Please enter your contact information"
+            check = false
+        }
+        return check
+    }
 
 }
--- app/src/main/java/com/wdfc/chattingapp/fragments/view_profiles.kt
+++ app/src/main/java/com/wdfc/chattingapp/fragments/view_profiles.kt
@@ -5,35 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.wdfc.chattingapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 
-class view_profiles : Fragment() {
-    // TODO: Rename and change types of parameters
+class view_profiles : Fragment(R.layout.fragment_view_profiles) {
+    var selected_sort = ""
+    var items = ArrayList<Array<String>>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val db = Firebase.firestore
 
 
+        // **********views initialisation************
+        var sort_spinner = activity?.findViewById<Spinner>(R.id.spinner3)
+        var list_view = activity?.findViewById<ListView>(R.id.list_view_profiles)
+        var search = activity?.findViewById<Button>(R.id.btn_search)
+        //***********views initialisation************
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-
-
-        arguments?.let {
-
+        //******spinner*********
+        var stateNames = arrayOf("Andhra Pradesh","Andaman and Nicobar" ,"Arunachal Pradesh", "Assam", "Bihar","Chandigarh", "Chattisgarh","Dadra and Nagar Haveli & Daman and Diu","Delhi",
+                "Goa", "Gujarat", "Haryana", "Himachal Pradesh","Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala","Ladakh", "Lakhswadeep", "Madhya Pradesh",
+                "Maharashtra" , "Manipur" , "Meghalaya" , "Mizoram" , "Nagaland" , "Odisha", "Puducherry","Punjab","Rajasthan", "Sikkim", "Tamil Nadu" , "Telangana" , "Tripura" , "Uttar Pradesh",
+                "Uttarakhand" , "West Bengal"
+        )
+        val spinner_adapter = activity?.let {
+            ArrayAdapter<String>(
+                    it,
+                android.R.layout.simple_spinner_dropdown_item,
+                stateNames
+        )
         }
-    }
+        sort_spinner?.adapter = spinner_adapter
+        sort_spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selected_sort = sort_spinner?.selectedItem.toString()
+            }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_profiles, container, false)
-    }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+            //******spinner*********
+
+            search?.setOnClickListener {
+                var thiefs = db.collection("lifesupport").whereEqualTo("state" , selected_sort).get()
+                        .addOnSuccessListener { document ->
+                            for ( i in document){
+                                items.add(arrayOf(i.get("name").toString() ,i.get("city").toString() ))
 
 
-}
+                            }
+                Toast.makeText(activity , items[0][0] , Toast.LENGTH_SHORT).show()
+                            items = ArrayList()
+            }
+
+
+
+
+
+
+
+
+
+
+}}}
